7a898fa04d00cd146e4257181b241cbddbec4370
markvangenderen <mark2.v@hotmail.com>
2021-07-19 22:54:00 -0700

    added launch sequence6

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 
 const WebSocket = require('ws');
-
 const port = process.env.PORT || 8080;
+
+
+function heartbeat() {
+    this.isAlive = true;
+}
+
+
 
 const wss = new WebSocket.Server({ port: port });
 
 wss.on('connection', function connection(ws) {
+  ws.isAlive = true;
+  ws.on('pong', heartbeat);
   ws.on('message', function incoming(data) {
     console.log(data);
     wss.clients.forEach(function each(client) {
@@ -14,4 +22,19 @@ wss.on('connection', function connection(ws) {
       }
     });
   });
+});
+
+
+const interval = setInterval(function ping() {
+    wss.clients.forEach(function each(ws) {
+      if (ws.isAlive === false) return ws.terminate();
+  
+      ws.isAlive = false;
+      ws.ping(noop);
+    });
+}, 30000);
+
+  
+wss.on('close', function close() {
+    clearInterval(interval);
 });
